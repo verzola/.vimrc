fe450bd0933da7aa6e8524ba5d5d6762f0cb2c35
Gustavo Verzola <gustavo@verzola.net>
2015-10-28 10:12:04 -0200

    Usa curl se não tiver wget

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 # Verifica se tem Git
 command -v git >/dev/null 2>&1 || { echo >&2 "I require git but it's not installed.  Aborting."; exit 1; }
 
+# Função para baixar
+download() {
+    if hash wget 2>/dev/null; then
+        wget "$@"
+    else
+        curl -O "$@"
+    fi
+}
+
 # Instala o Vundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
@@ -10,8 +19,11 @@ git clone https://github.com/verzola/.vimrc.git ~/git/vimrc
 # Cria link simbólico do .vimrc
 ln -s ~/git/vimrc/.vimrc ~/.vimrc
 
+# Entra na pasta de temas
+cd ~/.vim/colors
+
 # Baixa o colorscheme itg_flat
-wget -P ~/.vim/colors https://raw.githubusercontent.com/cdmedia/itg_flat_vim/master/colors/itg_flat.vim
+download https://raw.githubusercontent.com/cdmedia/itg_flat_vim/master/colors/itg_flat.vim
 
 # Instala os plugins
 vim +PluginInstall +qall
