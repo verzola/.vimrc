77a1e059fc599cae6ffc04ebd7f33fdc56c0f80c
Gustavo Verzola <verzola@gmail.com>
2016-05-28 11:45:13 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,25 +10,31 @@ download() {
     fi
 }
 
-echo -e "\e[42mInstalando o Vundle...\e[49m"
+echo -e "\e[44mInstalando o Vundle...\e[49m"
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo -e "\e[42m✓\e[49m"
 
-echo -e "\e[42mBaixando configuração...\e[49m"
+echo -e "\e[44mBaixando configuração...\e[49m"
 git clone https://github.com/verzola/.vimrc.git ~/git/vimrc
+echo -e "\e[42m✓\e[49m"
 
-echo -e "\e[42mCriando link simbólico para configuração...\e[49m"
+echo -e "\e[44mCriando link simbólico para configuração...\e[49m"
 ln -s ~/git/vimrc/.vimrc ~/.vimrc
+echo -e "\e[42m✓\e[49m"
 
-echo -e "\e[42mCriando pasta de temas...\e[49m"
+echo -e "\e[44mCriando pasta de temas...\e[49m"
 if [ ! -d ~/.vim/colors ];then
     mkdir ~/.vim/colors
 fi
+echo -e "\e[42m✓\e[49m"
 
 # Entra na pasta de temas
 cd ~/.vim/colors
 
-echo -e "\e[42mBaixando tema itg_flat...\e[49m"
+echo -e "\e[44mBaixando tema itg_flat...\e[49m"
 download https://raw.githubusercontent.com/cdmedia/itg_flat_vim/master/colors/itg_flat.vim
+echo -e "\e[42m✓\e[49m"
 
-echo -e "\e[42mInstalando plugins...\e[49m"
+echo -e "\e[44mInstalando plugins...\e[49m"
 vim +PluginInstall +qall
+echo -e "\e[42m✓\e[49m"
