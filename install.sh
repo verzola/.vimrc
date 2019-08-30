bd8e3a07b71d3ff1f615742f100469a9acb0e82d
Gustavo Verzola <verzola@gmail.com>
2019-08-30 15:14:20 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
+set -e
+
+mkdir -p ~/projects/verzola
 
 echo "Installing vim-plug..."
 if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]; then
-    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 echo "✓"
 
-if [ ! -d "~/projects" ]; then
-  echo "Creating ~/projects folder..."
-  mkdir ~/projects
-  echo "✓"
-fi
-if [ ! -d ~/projects/vimrc/ ]; then
+if [ ! -d ~/projects/verzola/vimrc ]; then
     echo "Cloning verzola's .vimrc..."
-    git clone https://github.com/verzola/.vimrc.git ~/projects/vimrc
+    git clone https://github.com/verzola/.vimrc.git ~/projects/verzola/vimrc
     echo "✓"
 else
     echo "Updating verzola's .vimrc..."
-    cd ~/projects/vimrc
-    git pull origin master
-    cd ~
+    git -C ~/projects/verzola/vimrc pull origin master
     echo "✓"
 fi
 
